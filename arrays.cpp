8675eba5897812d6098b4e942ad14317b7b72eaf
johnkilonzo <johnkilonzo435@gmail.com>
2025-09-21 18:58:06 +0300

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{int m[5];
+int sum=0;
+cout<<"enter the m arrays:\n";
+for(int i=i;1<=5;i++)
+{
+cin>>m[i];
+sum=sum+m[i];	
+}
+cout<<"average="<<sum/5<<endl;
+
+return 0;		
+}
